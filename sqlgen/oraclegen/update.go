6831e5c89b17d9331cdc9b12813593d68a888e85
Ryan Bastic <ryan@bastic.net>
2017-08-28 17:47:26 +0200

    fix UPDATE handling, should handle NULLs now, etc.

--- sqlgen/oraclegen/update.go
+++ sqlgen/oraclegen/update.go
@@ -11,51 +11,72 @@ import (
 
 // BindingUpdate generates the SQL for a given UPDATE statement for oracle with binding parameter values
 func (g Generator) BindingUpdate(sch *schema.Schema, obj *object.Object) (string, []interface{}, []interface{}, error) {
-	schTable := sch.GetTable(obj.Type)
-	if schTable == nil {
+	schTbl := sch.GetTable(obj.Type)
+	if schTbl == nil {
 		return "", nil, nil, errors.New("BindingUpdate: Table map unavailable for table " + obj.Type)
 	}
 
-	fieldsMap := schTable.Fields
+	fieldsMap := schTbl.Fields
 	if fieldsMap == nil {
 		return "", nil, nil, errors.New("BindingUpdate: Field map unavailable for table " + obj.Type)
 	}
 
-	whereClause, bindWhere, err := renderUpdateWhereClause(schTable, fieldsMap, obj)
+	whereClause, bindWhere, err := renderUpdateWhereClause(schTbl, fieldsMap, obj)
 	if err != nil {
 		return "", nil, nil, err
 	}
 
-	bindArgs := make([]interface{}, len(obj.KV)-1)
-	// TODO: - 1 for Oracle because we expect an identity field
-	newValuesAry := make([]string, len(obj.KV)-1)
 	i := 0
 
+	var bindArgs []interface{}
+	var newValuesAry []string
+
+	// If some things have changed, then only use fields that we're sure have changed
 	if len(obj.ChangedFields) > 0 {
+		bindArgs = make([]interface{}, len(obj.ChangedFields))
+		newValuesAry = make([]string, len(obj.ChangedFields))
+
 		for k := range obj.ChangedFields {
-			f := fieldsMap[k]
+			f := schTbl.GetField(k)
 			if f.IsIdentity {
 				continue
 			}
-			newValuesAry[i] = fmt.Sprintf("%s = %s%d", f.Name, renderBindingUpdateValue(f), i)
-			bindArgs[i] = obj.KV[k]
-
+			v := obj.KV[k]
+			if v == nil {
+				newValuesAry[i] = fmt.Sprintf("%s = NULL", f.Name)
+				bindArgs[i] = nil
+			} else {
+				newValuesAry[i] = fmt.Sprintf("%s = %s%d", f.Name, renderBindingUpdateValue(f), i)
+				bindArgs[i] = v
+			}
 			i++
 		}
 	} else {
+		// An update where it's not explicitly clear that anything has changed should
+		// just set every field we have available.
+		bindArgs = make([]interface{}, len(obj.KV)-1)
+		// TODO: -1 for Oracle because we expect an identity field
+		newValuesAry = make([]string, len(obj.KV)-1)
+
 		for k, v := range obj.KV {
-			f := fieldsMap[k]
+			f := schTbl.GetField(k)
 			if f.IsIdentity {
 				continue
 			}
-			newValuesAry[i] = fmt.Sprintf("%s = %s%d", f.Name, renderBindingUpdateValue(f), i)
-			bindArgs[i] = v
+			if v == nil {
+				newValuesAry[i] = fmt.Sprintf("%s = NULL", f.Name)
+				bindArgs[i] = nil
+			} else {
+				newValuesAry[i] = fmt.Sprintf("%s = %s%d", f.Name, renderBindingUpdateValue(f), i)
+				bindArgs[i] = v
+			}
 
 			i++
 		}
 	}
+	bindArgs = removeNilsIfNeeded(bindArgs)
 
-	tableName := schema.GetTableName(schTable.Name, obj.Type)
+	tableName := schema.GetTableName(schTbl.Name, obj.Type)
 	sqlStr := fmt.Sprintf("UPDATE %s SET %s WHERE %s", tableName, strings.Join(newValuesAry, ","), whereClause)
 	return sqlStr, bindArgs, bindWhere, nil
 }
