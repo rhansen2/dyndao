b1ee669081eb171068495ddf2617815e19c95e23
Ryan Bastic <ryan@bastic.net>
2017-10-13 17:49:19 +0200

    split Insert and Update into their own files

---
+++ orm/update.go
@@ -0,0 +1,82 @@
+package orm
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/inconshreveable/log15"
+	"github.com/pkg/errors"
+	"github.com/rbastic/dyndao/object"
+)
+
+
+// Update function will UPDATE a record ...
+func (o ORM) Update(ctx context.Context, tx *sql.Tx, obj *object.Object) (int64, error) {
+	errorString := "Update error"
+
+	select {
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	default:
+	}
+
+	err := o.CallBeforeUpdateHookIfNeeded( obj )
+	if err != nil {
+		if os.Getenv("DEBUG_UPDATE") != "" {
+			log15.Error(errorString, "BeforeUpdateHookError", err)
+		}
+		return 0, err
+	}
+
+	sqlStr, bindArgs, bindWhere, err := o.sqlGen.BindingUpdate(o.s, obj)
+	if err != nil {
+		if os.Getenv("DEBUG_UPDATE") != "" {
+			fmt.Println("Update/sqlStr, err=", err)
+		}
+		return 0, err
+	}
+	if os.Getenv("DEBUG_UPDATE") != "" {
+		fmt.Println("Update/sqlStr=", sqlStr, "bindArgs=", bindArgs, "bindWhere=", bindWhere)
+	}
+
+	stmt, err := stmtFromDbOrTx(ctx, o, tx, sqlStr)
+	if err != nil {
+		return 0, err
+	}
+	defer func() {
+		err := stmt.Close()
+		if err != nil {
+			fmt.Println(err) // TODO logging implementation
+		}
+	}()
+
+	allBind := append(bindArgs, bindWhere...)
+	newAllBind := make([]interface{}, len(allBind))
+	for i, arg := range allBind {
+		newAllBind[i] = maybeDereferenceArgs(arg)
+	}
+	res, err := stmt.ExecContext(ctx, newAllBind...)
+	if err != nil {
+		return 0, errors.Wrap(err, "Update")
+	}
+
+	rowsAff, err := res.RowsAffected()
+	if err != nil {
+		return 0, err
+	}
+
+	err = o.CallAfterUpdateHookIfNeeded( obj )
+	if err != nil {
+		if os.Getenv("DEBUG_UPDATE") != "" {
+			log15.Error(errorString, "BeforeAfterUpdateHookError", err)
+		}
+		return 0, err
+	}
+
+	obj.SetSaved(true)       // Note that the object has been recently saved
+	obj.ResetChangedFields() // Reset the 'changed fields', if any
+
+	return rowsAff, nil
+}
