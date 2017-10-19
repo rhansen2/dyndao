45a18507ea4afee5f5d9bd09cfa74c76f2178f38
Ryan Bastic <ryan@bastic.net>
2017-10-19 20:52:22 +0200

    add numRows to FindOrCreate return values

--- orm/find_or_create.go
+++ orm/find_or_create.go
@@ -7,35 +7,35 @@ import (
 	"github.com/rbastic/dyndao/object"
 )
 
-func (o ORM) FindOrCreateTx(ctx context.Context, tx *sql.Tx, obj *object.Object) (*object.Object, error) {
+func (o ORM) FindOrCreateTx(ctx context.Context, tx *sql.Tx, obj *object.Object) (int64, *object.Object, error) {
 	obj, err := o.RetrieveTx(ctx, tx, obj.Type, obj.KV)
 	if err != nil {
-		return nil, err
+		return 0, nil, err
 	}
 
 	if obj == nil {
 		numRows, err := o.Insert(ctx, tx, obj)
 		if err != nil {
-			return nil, err
+			return numRows, nil, err
 		}
 		if numRows == 0 {
-			return nil, errors.New("FindOrCreateTx: numRows was 0 when expecting Insert")
+			return 0, nil, errors.New("FindOrCreateTx: numRows was 0 when expecting Insert")
 		}
 
-		return obj, nil
+		return numRows, obj, nil
 	}
 
-	return obj, nil
+	return 0, obj, nil
 }
 
-func (o ORM) FindOrCreate(ctx context.Context, obj *object.Object) (*object.Object, error) {
+func (o ORM) FindOrCreate(ctx context.Context, obj *object.Object) (int64, *object.Object, error) {
 	return o.FindOrCreateTx(ctx, nil, obj)
 }
 
-func (o ORM) FindOrCreateKVTx(ctx context.Context, tx *sql.Tx, typ string, queryKV map[string]interface{}, createKV map[string]interface{}) (*object.Object, error) {
+func (o ORM) FindOrCreateKVTx(ctx context.Context, tx *sql.Tx, typ string, queryKV map[string]interface{}, createKV map[string]interface{}) (int64, *object.Object, error) {
 	obj, err := o.RetrieveTx(ctx, tx, typ, queryKV)
 	if err != nil {
-		return nil, err
+		return 0, nil, err
 	}
 
 	if obj == nil {
@@ -44,18 +44,18 @@ func (o ORM) FindOrCreateKVTx(ctx context.Context, tx *sql.Tx, typ string, query
 
 		numRows, err := o.Insert(ctx, tx, obj)
 		if err != nil {
-			return nil, err
+			return numRows, nil, err
 		}
 		if numRows == 0 {
-			return nil, errors.New("FindOrCreateKVTx: numRows was 0 when expecting Insert")
+			return 0, nil, errors.New("FindOrCreateKVTx: numRows was 0 when expecting Insert")
 		}
 
-		return obj, nil
+		return numRows, obj, nil
 	}
 
-	return obj, nil
+	return 0, obj, nil
 }
 
-func (o ORM) FindOrCreateKV(ctx context.Context, typ string, queryKV map[string]interface{}, createKV map[string]interface{}) (*object.Object, error) {
+func (o ORM) FindOrCreateKV(ctx context.Context, typ string, queryKV map[string]interface{}, createKV map[string]interface{}) (int64, *object.Object, error) {
 	return o.FindOrCreateKVTx(ctx, nil, typ, queryKV, createKV)
 }
