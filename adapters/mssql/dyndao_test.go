7f621ba2682a32cacb21e6eee066f7d8ea965735
Ryan Bastic <ryan@bastic.net>
2017-11-13 12:49:46 +0100

    add race rules

--- adapters/mssql/dyndao_test.go
+++ adapters/mssql/dyndao_test.go
@@ -29,7 +29,6 @@ func GetDB() *sql.DB {
 	if dsn == "" {
 		panic("MSSQL_DSN environment variable is not set, cannot initialize database")
 	}
-	fmt.Println("Opening with dsn: ", dsn)
 	db, err := sql.Open("mssql", dsn)
 	if err != nil {
 		panic(err)
